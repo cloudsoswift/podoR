233dc0aec367a7acef6b99dc2309d6673c4a1fc8
박규영 <magic9282m@gmail.com>
2025-11-22 20:56:55 +0900

    init: Spring Boot Project Setup

---
+++ backend/src/main/java/cloudsoswift/podoR/PodoRApplication.java
@@ -0,0 +1,13 @@
+package cloudsoswift.podoR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodoRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodoRApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/cloudsoswift/podoR/PodoRApplicationTests.java
@@ -0,0 +1,13 @@
+package cloudsoswift.podoR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodoRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
